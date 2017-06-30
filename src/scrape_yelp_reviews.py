7654ade54dc185c1d80aee1db32daa65c6ab89de
Samuel Lee <samuel@alumni.caltech.edu>
2017-06-30 01:42:27 -0700

    scraping mostly implemented

--- src/scrape_yelp_reviews.py
+++ src/scrape_yelp_reviews.py
@@ -5,6 +5,43 @@ import numpy as np
 import time
 import random
 from pymongo import MongoClient
+import queue
+import threading
+
+
+class Worker(threading.Thread):
+    def __init__(self, thread_id, q, queue_lock):
+        super(Worker, self).__init__()
+        self.thread_id = thread_id
+        self.q = q
+        self.queue_lock = queue_lock
+        self._stop_event = threading.Event()
+
+    def run(self):
+        print('Thread[{}] starting...'.format(self.thread_id))
+        process_data(self)
+        print('Thread[{}] exiting...'.format(self.thread_id))
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+
+def process_data(worker):
+    thread_id = worker.thread_id
+    q = worker.q
+    queue_lock = worker.queue_lock
+    while not worker.stopped():
+        queue_lock.acquire()
+        if not q.empty():
+            data = q.get()
+            queue_lock.release()
+            scrape_reviews(thread_id, *data)
+        else:
+            queue_lock.release()
+        time.sleep(1)
 
 
 def load_restaurant_ids(spark):
@@ -16,6 +53,14 @@ def load_restaurant_ids(spark):
             .toLocalIterator()
         )
     ]
+
+
+def get_all_max_page_saved(reviews_table):
+    saved_yelp_data = reviews_table.aggregate(
+        [{'$group': {'_id': '$yelp_id', 'max_page': {'$max': '$page'}}}]
+    )
+
+    return {item['_id'] : item['max_page'] for item in saved_yelp_data}
 
 
 def get_user_agents_with_probs():
@@ -66,11 +111,11 @@ def add_session_proxy(s):
 def create_requests_session(probs, user_agents):
     # choose random user_agent string
     user_agent = np.random.choice(user_agents, p=probs)
-    print('user_agent: {0}'.format(user_agent))
+    # print('user_agent: {0}'.format(user_agent))
     s = requests.Session()
     s.headers['User-Agent'] = user_agent
 
-    # s = add_session_proxy(s)
+    s = add_session_proxy(s)
 
     return s
 
@@ -92,7 +137,23 @@ def save_html(reviews_table, yelp_id, page, html_str):
         }
     )
 
-def scrape_reviews(yelp_id, probs, user_agents, reviews_table):
+
+def get_random_sleep_time():
+    sleep_time = 0
+    while sleep_time < 2:
+        mean = np.random.normal(10, 2)
+        stdev = np.random.normal(3, 0.3)
+        sleep_time = np.random.normal(mean, stdev)
+    return sleep_time
+
+
+def scrape_reviews(thread_id, yelp_id, max_page_saved, probs, user_agents,
+        reviews_table):
+    sleep_time = get_random_sleep_time()
+    # print('Thread[{}]: Sleep {} seconds ...'
+    #     .format(thread_id, sleep_time))
+    time.sleep(sleep_time)
+
     s = create_requests_session(probs, user_agents)
 
     base_url = 'https://www.yelp.com/biz/{0}'
@@ -101,36 +162,47 @@ def scrape_reviews(yelp_id, probs, user_agents, reviews_table):
     # url = 'https://lumtest.com/echo.json' # test url to confirm proxy working
     params = {}
 
-    print('Getting page 0 ...')
+    # print('Thread[{}]: page: 0 yelp_id: {}'
+    #     .format(thread_id, yelp_id))
     response = get(s, url, params)
     html_str = response.text
-    save_html(reviews_table, yelp_id, 0, html_str)
 
-    print('Scraping number of pages ...')
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # print('Thread[{}]: Scraping number of pages ...'
+    #     .format(thread_id))
+    soup = BeautifulSoup(html_str, 'html.parser')
     num_pages = int(
         soup.find('div', class_='page-of-pages')
         .getText()
         .split()[3]
     )
-    print('yelp_id: {0} num_pages: {1}'.format(yelp_id, num_pages))
+    print('Thread[{}]: num_pages: {} saved: {} yelp_id: {}'
+        .format(thread_id, num_pages, max_page_saved + 1, yelp_id))
 
-    for page in range(1, num_pages):
-        mean = np.random.normal(10, 2)
-        stdev = np.random.normal(3, 0.3)
-        sleep_time = np.random.normal(mean, stdev)
-        print('Sleeping for {0} seconds ...'.format(sleep_time))
+    if max_page_saved is None:
+        save_html(reviews_table, yelp_id, 0, html_str)
+        max_page_saved = 0
+
+    for page in range(max_page_saved + 1, num_pages):
+        sleep_time = get_random_sleep_time()
+        # print('Thread[{}]: Sleep {} seconds ...'
+        #     .format(thread_id, sleep_time))
         time.sleep(sleep_time)
-        start = reviews_per_page * page
-        params['start'] = start
-        print('Getting page {0} ...'.format(page))
+        params['start'] = reviews_per_page * page
+        print('Thread[{}]: page: {}/{} yelp_id: {}'
+            .format(thread_id, page + 1, num_pages, yelp_id))
         response = get(s, url, params)
-
         html_str = response.text
         save_html(reviews_table, yelp_id, page, html_str)
 
-    exit()
 
+def create_worker_threads(num_threads, work_queue, queue_lock):
+    # Create new threads
+    threads = []
+    for thread_id in range(num_threads):
+        thread = Worker(thread_id, work_queue, queue_lock)
+        thread.start()
+        threads.append(thread)
+    return threads
 
 
 def main():
@@ -145,9 +217,48 @@ def main():
         .getOrCreate()
     )
     yelp_ids = load_restaurant_ids(spark)
+
+    all_max_page_saved = get_all_max_page_saved(reviews_table)
+    # print(all_max_page_saved)
+
+    print('Total Restaurants: {0}'.format(len(yelp_ids)))
+    print('Total Restaurants Saved: {0}'.format(len(all_max_page_saved)))
+    print('Total Restaurants Left: {0}'.format(
+        len(yelp_ids) - len(all_max_page_saved)))
+
     probs, user_agents = get_user_agents_with_probs()
-    for yelp_id in yelp_ids[:1]:
-        scrape_reviews(yelp_id, probs, user_agents, reviews_table)
+
+    num_threads = 50
+    work_queue = queue.Queue()
+    queue_lock = threading.Lock()
+    threads = create_worker_threads(num_threads, work_queue, queue_lock)
+
+    # Fill the work queue
+    for yelp_id in yelp_ids:
+        if yelp_id in all_max_page_saved:
+            max_page_saved = all_max_page_saved[yelp_id]
+        else:
+            max_page_saved = None
+        data = (yelp_id, max_page_saved, probs, user_agents, reviews_table)
+        queue_lock.acquire()
+        work_queue.put(data)
+        queue_lock.release()
+
+    print('Done filling queue')
+    # Wait for work queue to empty
+    while not work_queue.empty():
+        pass
+    print('queue is empty!')
+    print('Notify threads it is time to exit')
+    # Notify threads it's time to exit
+    for t in threads:
+        t.stop()
+    print('waiting for threads to complete')
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    print('Exiting main thread ...')
 
 
 if __name__ == '__main__':
