82ded3c2994aa51fe6076e557e8df4521718123a
Samuel Lee <samuel@alumni.caltech.edu>
2017-07-12 11:26:05 -0700

    work on website

--- src/app.py
+++ src/app.py
@@ -1,13 +1,38 @@
 from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import pyspark as ps
 
 app = Flask(__name__)
 PORT = 5353
 
-@app.route('/recommend', methods=['POST'])
+with open('../data/item_factors.pkl', 'rb') as f:
+    item_factors = pickle.load(f)
+
+with open('../data/item_ids.pkl', 'rb') as f:
+    item_ids = pickle.load(f)
+
+spark = (
+    ps.sql.SparkSession.builder
+    # .master("local[8]")
+    .appName("webapp")
+    .getOrCreate()
+)
+
+# Load restaurant metadata
+restaurants_df = spark.read.parquet('../data/restaurants')
+
+
+@app.route('/recommend', methods=['POST', 'GET'])
 def recommend():
     json_doc = request.json
 
     return 'Hello world! Hoping to recommend stuff here'
+
+@app.route('/check')
+def check():
+    restaurants_df.printSchema()
+    return 'Hi!'
 
 
 @app.route('/')
