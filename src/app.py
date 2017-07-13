ba79051a24567e1f4955ee362bfd0e2a0a5076d7
Samuel Lee <samuel@alumni.caltech.edu>
2017-07-13 13:50:02 -0700

    implement stars and search form

--- src/app.py
+++ src/app.py
@@ -6,11 +6,29 @@ import pyspark as ps
 app = Flask(__name__)
 PORT = 5353
 
+with open('../data/item_factors.pkl', 'rb') as f:
+    item_factors = pickle.load(f)
+
+with open('../data/item_ids.pkl', 'rb') as f:
+    item_ids = pickle.load(f)
+
+spark = (
+    ps.sql.SparkSession.builder
+    # .master("local[8]")
+    .appName("webapp")
+    .getOrCreate()
+)
+
+# Load restaurant metadata
+restaurants_df = spark.read.parquet('../data/restaurants')
+
+
 @app.route('/recommend', methods=['POST', 'GET'])
 def recommend():
     json_doc = request.json
 
     return 'Hello world! Hoping to recommend stuff here'
+
 
 @app.route('/check')
 def check():
@@ -23,23 +41,7 @@ def index():
     return render_template('index.html')
 
 
-def main():
-    with open('../data/item_factors.pkl', 'rb') as f:
-        item_factors = pickle.load(f)
-
-    with open('../data/item_ids.pkl', 'rb') as f:
-        item_ids = pickle.load(f)
-
-    spark = (
-        ps.sql.SparkSession.builder
-        # .master("local[8]")
-        .appName("webapp")
-        .getOrCreate()
-    )
-
-    # Load restaurant metadata
-    restaurants_df = spark.read.parquet('../data/restaurants')
-    
+def main():    
     # Start Flask app
     app.run(host='0.0.0.0', port=PORT, debug=True)
 
