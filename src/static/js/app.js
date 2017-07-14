4dda11bcf970d5e8139d608ff254970e4f28e65a
Samuel Lee <samuel@alumni.caltech.edu>
2017-07-14 01:39:44 -0700

    work on search ui

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -20,20 +20,25 @@ $( "#searchForm" ).submit(function( event ) {
 
     $('#results').empty()
     $.each(data, function(k, v) {
-      $('#results').append(
+      html = (
         '<div class="row search-result">'+
           '<div class="col-xs-2 vcenter"><img src="'+v.image_url+'"></div>'+
           '<div class="col-xs-4 vcenter"><a href="'+v.url+'" target="_blank">'+v.name+'</a></div>' +
-          '<div class="col-xs-4 vcenter">'+
-            '<div class="address">'+v.location[5][0]+'</div>'+
-            '<div class="address">'+v.location[5][1]+'</div></div>'+
-          '<div class="col-xs-2 vcenter">'+
+          '<div class="col-xs-5 vcenter">'
+      );
+      $.each(v.location[5], function(i, loc_val) {
+        html += '<div class="address">'+loc_val+'</div>'
+      });
+      html += (
+          '</div>'+
+          '<div class="col-xs-1 vcenter">'+
             '<input type="number" id="'+k+'" class="rating rating-input" data-clearable="" '+
             'data-icon-lib="fa" data-active-icon="fa-star" data-inactive-icon="fa-star-o" '+
             'data-clearable-icon="fa-trash-o" />'+
           '</div>'+
         '</div>'
       );
+      $('#results').append(html)
     });
     $('input.rating').rating({});
   });
