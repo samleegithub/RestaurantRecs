2fb8dbba2d217fec2ae318d52d382a578a8eb67d
Samuel Lee <samuel@alumni.caltech.edu>
2017-07-17 00:52:30 -0700

    finished with your ratings ui

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,36 +1,36 @@
-var $searchLoading = $('#search-loading');
-var $searchResults = $('#search-results');
-var $selectedRatingsGroup = $('.selected-ratings-group');
-var $selectedRatings = $('#selected-ratings');
-var all_ratings = {}
-var valid_rating_values = {1:true, 2:true, 3:true, 4:true, 5:true}
+const $searchLoading = $('#search-loading');
+const $searchResults = $('#search-results');
+const $selectedRatingsGroup = $('.selected-ratings-group');
+const $selectedRatings = $('#selected-ratings');
+let all_ratings = {};
+const valid_rating_values = {1:true, 2:true, 3:true, 4:true, 5:true};
 
 // Attach a submit handler to the form
-$("#searchForm").submit(function(event) {
- 
+$('#searchForm').submit(function(event) {
+
   // Stop form from submitting normally
-  event.preventDefault();
- 
+  event.preventDefault()
+
   // Get some values from elements on the page:
   var $form = $(this);
-  var keyword = $form.find("input[name='keyword']").val();
-  var location = $form.find("input[name='location']").val();
-  var url = $form.attr("action");
- 
+  var keyword = $form.find('input[name="keyword"]').val();
+  var location = $form.find('input[name="location"]').val();
+  var url = $form.attr('action');
+
   $searchResults.empty()
   $searchLoading.show()
 
   // Send the data using post
   var posting = $.post(url, { keyword: keyword, location: location }, dataType='json');
- 
+
   // Put the results in a div
   posting.done(function(data) {
-    var content = $(data);
-    // console.log(content)
+    // console.log(data)
 
     $searchLoading.hide()
-    
+
     $.each(data, function(k, v) {
+      var str_id = k;
       var model_id = v['model_id'];
       var name = v['name'];
       var image_url = v['image_url'];
@@ -40,8 +40,9 @@ $("#searchForm").submit(function(event) {
         '<div class="row search-result-row">'+
           '<div class="col-xs-12 col-sm-2 cell search-results-img-div">'
       );
-      if (typeof image_url != 'undefined' && image_url != '')
-          html += '<img class="search-results-img" src="'+image_url+'" />'
+      if (typeof image_url != 'undefined' && image_url != '') {
+        html += '<img class="search-results-img" src="'+image_url+'" />'
+      }
       html += (
           '</div>' +
           '<div class="col-xs-12 col-sm-4 cell">'+
@@ -50,7 +51,7 @@ $("#searchForm").submit(function(event) {
               name+'</a>'+
           '</div>'+
           '<div class="col-xs-12 col-sm-4 cell">'
-      );
+      )
       $.each(location, function(i, location_line) {
         html += '<div class="address">'+location_line+'</div>'
       });
@@ -61,8 +62,9 @@ $("#searchForm").submit(function(event) {
               'id="search_'+model_id+'" '+
               'name="search-rating" '
       )
-      if (model_id in all_ratings)
-        html += 'value="'+all_ratings[model_id]['rating']+'" '
+      if (str_id in all_ratings) {
+        html += 'value="'+all_ratings[str_id]['rating']+'" '
+      }
       html += (
               'data-clearable="" data-inline '+
               'data-icon-lib="fa" data-active-icon="fa-star" '+
@@ -70,19 +72,27 @@ $("#searchForm").submit(function(event) {
               'data-clearable-icon="fa-trash-o" />'+
           '</div>'+
         '</div>'
-      );
+      )
       $searchResults.append(html)
+      v['str_id'] = k
       $('#search_'+model_id).data('values', v)
-    });
+    })
     $('input.search-rating').rating({});
-  });
-});
+  })
+})
 
 function update_user_ratings() {
   $selectedRatings.empty()
+  var all_ratings_keys = []
   $.each(all_ratings, function(k, v) {
-    if (typeof k != 'undefined' && k != '')
-      var rating = v['rating']
+    all_ratings_keys.push(k)
+  })
+
+  $.each(all_ratings_keys.sort(), function(i) {
+    var k = all_ratings_keys[i];
+    if (typeof k != 'undefined' && k != '') {
+      var v = all_ratings[k];
+      var rating = v['rating'];
       var model_id = v['model_id'];
       var name = v['name'];
       var image_url = v['image_url'];
@@ -92,8 +102,9 @@ function update_user_ratings() {
         '<div class="row rating-row">'+
           '<div class="col-xs-12 col-sm-2 vcenter cell rating-img-div">'
       );
-      if (typeof image_url != 'undefined' && image_url != '')
+      if (typeof image_url != 'undefined' && image_url != '') {
           html += '<img class="rating-img" src="'+image_url+'" />'
+      }
       html += (
           '</div>' +
           '<div class="col-xs-12 col-sm-4 vcenter cell">'+
@@ -101,10 +112,10 @@ function update_user_ratings() {
               name+'</a>'+
           '</div>'+
           '<div class="col-xs-12 col-sm-4 vcenter cell">'
-      );
-    $.each(location, function(i, location_line) {
-      html += '<div class="address">'+location_line+'</div>'
-    });
+      )
+      $.each(location, function(i, location_line) {
+        html += '<div class="address">'+location_line+'</div>'
+      })
       html += (
           '</div>'+
           '<div class="col-xs-12 col-sm-2 cell">'+
@@ -118,58 +129,71 @@ function update_user_ratings() {
               'data-clearable-icon="fa-trash-o" />'+
           '</div>'+
         '</div>'
-      );
+      )
       $selectedRatings.append(html)
       $('#rating_'+model_id).data('values', v)
-  });
+    }
+  })
   $('input.user-rating').rating({});
 
   $('input.user-rating').change(function() {
     // console.log('input.rating changed')
 
     var rating = $(this).val();
-    var v = $(this).data('values')
-    var model_id = v.model_id
-    var $search_rating = $('#search_'+model_id)
+    var v = $(this).data('values');
+    var str_id = v.str_id;
+    var model_id = v.model_id;
+    var $search_rating = $('#search_'+model_id);
     if (rating in valid_rating_values) {
       v['rating'] = rating
-      all_ratings[model_id] = v
+      all_ratings[str_id] = v
       // update search result ratings if found
       if (typeof $search_rating != 'undefined' && $search_rating != '') {
         $search_rating.val(rating)
         $search_rating.prev().children().first().trigger('mouseout')
       }
-    } else if (model_id in all_ratings) {
-      delete all_ratings[model_id];
+    } else if (str_id in all_ratings) {
+      delete all_ratings[str_id]
       // update search result ratings if found
-      if (typeof $search_rating != 'undefined' && $search_rating != '') {
+      if ($search_rating.length) {
+        // console.log('found matching search rating')
+        // console.log($search_rating)
         $search_rating.prev().children().last().trigger('click')
+      } else {
+        // console.log('no matching search rating found')
+        update_user_ratings()
       }
     }
-    // console.log(all_ratings)
-    update_user_ratings()
-  });
+    // console.log('user-rating change: '+all_ratings)
+    // update_user_ratings()
+  })
 
+  // console.log(all_ratings)
+  // console.log($.isEmptyObject(all_ratings))
 
-  $selectedRatingsGroup.show();
+  if ($.isEmptyObject(all_ratings)) {
+    $selectedRatingsGroup.hide()
+  } else {
+    $selectedRatingsGroup.show();
+  }
 }
 
 $(document).ajaxComplete(function() {
   // fire when any Ajax requests complete
   $('input.search-rating').change(function() {
     // console.log('input.rating changed')
-    $('input.search-rating').each(function() {
-      var rating = $(this).val();
-      var v = $(this).data('values')
-      var model_id = v.model_id
-      if (rating in valid_rating_values) {
-        v['rating'] = rating
-        all_ratings[model_id] = v
-      } else if (model_id in all_ratings) {
-        delete all_ratings[model_id];
-      }
-    });
+    // $('input.search-rating').each(function() {
+    var rating = $(this).val();
+    var v = $(this).data('values');
+    var str_id = v['str_id'];
+    if (rating in valid_rating_values) {
+      v['rating'] = rating
+      all_ratings[str_id] = v
+    } else if (str_id in all_ratings) {
+      delete all_ratings[str_id];
+    }
+    // });
     // console.log(all_ratings)
     update_user_ratings()
-  });
-});
+  })
+})
